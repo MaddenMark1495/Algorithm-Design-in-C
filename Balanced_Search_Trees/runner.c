1a3c9e76e9adc6e12083a15ef66149a940be6e93
Mark Madden <mcmpw6@mail.missouri.edu>
2015-12-30 15:53:54 -0600

    name changes

--- Balanced_Search_Trees/runner.c
+++ Balanced_Search_Trees/runner.c
@@ -1,8 +1,8 @@
 //Mark Madden
-//14192986 
+//14192986
 //mcmpw6
 //lab 10
-//lab d 
+//lab d
 //balancedBST
 
 #include <stdio.h>
@@ -16,19 +16,20 @@ int main(int argc, char* argv[]){
 		printf("Not enough inputs");
 		return 0;
 		}
-	
+
 	int i;
 	int low;
 	int high;
 	int *a=malloc(sizeof(int));
 	//int a[100];
-	
+
 	BST* tree=NULL;
-	
+
 	FILE* file=fopen(argv[1],"r");	// open file
 	if(file==NULL){
 		printf("File could not be open\n");
 	}
+	
 	int size;
 	fscanf(file,"%d",&size);
 	for(i=0;i<size;i++){
@@ -37,11 +38,11 @@ int main(int argc, char* argv[]){
 	tree=build_balanced_tree(a,0,size-1);
 	print(tree);
 	//tree=delete_node(tree,atoi(argv[2]));
-	
+
 	printf("Tree without %d\n",atoi(argv[2]));
 	print(tree);
-	
+
 	//fclose(file);
 	return 0;
-	
+
 }
